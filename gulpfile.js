343bb70bd87a249329e2745cd469dc1e46777457
zenpudding <zenpudding@gmail.com>
2015-03-15 19:48:09 -0400

    structure editing

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var jade = require('gulp-jade');
 
+
+// Compile to HTML
+gulp.task('templates', function() {
+  gulp.src('./src/*.jade')
+    .pipe(jade({
+      pretty: true	
+    }))
+    .pipe(gulp.dest('./static/'))
+});
+
+// Compile sass to css
 gulp.task('sass', function () {
-    gulp.src('./scss/*.scss')
+    gulp.src('./src/scss/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('static/styles'));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'templates']);
