9db7f31a203afc7d0e6925456a10a515abcb5fa9
zenpudding <zenpudding@gmail.com>
2016-02-06 14:33:17 -0500

    Update 2016-02-06T19:33:13.418Z

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-var foo = false;
 
 $(document).ready(function(){
 
@@ -7,6 +6,23 @@ $('.mousezone').mouseover( function() {
 	$('.menu').addClass('active');
 }).mouseleave( function() {
 	$('.menu').removeClass('active');
+});
+
+//menu show/hide
+$(window).scroll(function(){
+	if($(window).scrollTop() > $(".splash").outerHeight() - 300){
+		$('#menu').addClass('active');
+	}
+	else {
+		$('#menu').removeClass('active');
+	}
+});
+
+//hover stuff for design blocks
+$('.design-container .block').mouseover( function() {
+	$(this).addClass('active');
+}).mouseleave( function() {
+	$(this).removeClass('active');
 });
 
 
