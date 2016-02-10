882248e7477ec2b820ab604b02ceca823377dfd5
zenpudding <zenpudding@gmail.com>
2016-02-09 19:27:37 -0500

    Update 2016-02-10T00:27:34.199Z

--- js/main.js
+++ js/main.js
@@ -25,5 +25,26 @@ $('.design-container .block').mouseover( function() {
 	$(this).removeClass('active');
 });
 
+//load modal on work page
+$('.ds-site').click(function(){
+	//event.preventDefault();
+	$('#modal').addClass('active');
+	$('#m-overlay').addClass('active');
+	setTimeout(function() {$('body').addClass('modal-open'); }, 500);
+	setTimeout(function() {$('#modal-content').load('/work/ds-site.html .case-container'); }, 500);
+	setTimeout(function() {$('#m-overlay').addClass('small'); }, 500);
+	setTimeout(function() {$('.m-close').addClass('active'); }, 500);
+	setTimeout(function() {$('#m-overlay').css('z-index', '0'); }, 500);
+});
+
+$('.m-close').click(function(){
+	$('#m-overlay').css('z-index', '999');
+	$('#m-overlay').removeClass('small');
+	setTimeout(function() {$('#modal-content').empty(); }, 500);
+	setTimeout(function() {$('.m-close').removeClass('active'); }, 500);
+	setTimeout(function() {$('#m-overlay').removeClass('active'); }, 1000);
+	setTimeout(function() {$('#modal').removeClass('active'); }, 1000);
+	setTimeout(function() {$('body').removeClass('modal-open') }, 500);
+});
 
 });
