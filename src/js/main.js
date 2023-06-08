a7f832a9969b3ccd4e4b86fe1e47f4f48324aeb4
zevin <zenpudding@gmail.com>
2023-06-07 23:16:47 -0400

    add nav expansion
    also media query between sm - md screens

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,14 @@
 
 $(document).ready(function(){
 
+  //nav expand controls
+  $('.title').click(function(){
+  	if($('nav').hasClass('expanded')) {
+  		$('nav').removeClass('expanded');
+  	}
+  	else {
+  		$('nav').addClass('expanded');
+  	}
+  });
+
 });
