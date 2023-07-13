5f6a2601a3f33378e26648a040d5c7fe3a6e431c
zevin <zenpudding@gmail.com>
2023-07-13 16:56:37 -0400

    added navigation and updated build structure
    - updated html to use partials
    - updated node-sass to dart sass
    - updated scss to use partials
    - added js for nav active states

--- src/js/main.js
+++ src/js/main.js
@@ -11,4 +11,23 @@ $(document).ready(function(){
   	}
   });
 
+  //active state on nav depending on page
+  switch(window.location.pathname) {
+    case '/':
+      clearNavActive();
+      $('#about-link').addClass('active');
+      break;
+    case '/bluemine.html':
+      clearNavActive();
+      $('#bluemine-link').addClass('active');
+  }
+
+  //clear nav active states
+  function clearNavActive() {
+    let navLinks = document.getElementsByClassName("nav-link-item");
+    for (let i = 0; i < navLinks.length; i++) {
+      navLinks[i].classList.remove("active");
+    }
+  }
+
 });
